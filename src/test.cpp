18fbb349b3cc6bc45a36f1b3958c5a3a3da8f996
Maarten van Gompel <proycon@anaproy.nl>
2013-10-30 16:38:34 +0100

    added

---
+++ src/test.cpp
@@ -0,0 +1,21 @@
+#include <pattern.h>
+
+using namespace std;
+
+void test(bool r) {
+    if (r) {
+        cerr << string( ".... ok") << endl;
+    } else {
+        cerr << ".... FAILED!" << endl;
+        exit(2);
+    }
+}
+
+
+
+int main( int argc, char *argv[] ) {
+	//string model = argv[1];
+	//string classfile = argv[1];
+    Pattern pattern = Pattern();
+    return 0;
+}
