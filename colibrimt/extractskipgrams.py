7db08f2ac81a76c94c06b50c2f9835037bc67e72
Maarten van Gompel <proycon@anaproy.nl>
2013-11-15 18:14:47 +0100

    fix

--- colibrimt/extractskipgrams.py
+++ colibrimt/extractskipgrams.py
@@ -54,7 +54,7 @@ def extractskipgrams(alignmodel, maxlength= 8, minskiptypes=2, tmpdir="./", quie
         if not isinstance(features, list) and not isinstance(features, tuple):
             print("WARNING: Expected feature vector, got " + str(type(features)),file=sys.stderr)
             continue
-        if not isinstance(features[-1], list) or not isinstance(features[-1], tuple):
+        if not isinstance(features[-1], list) and not isinstance(features[-1], tuple):
             print("WARNING: Word alignments missing for a pair, skipping....",file=sys.stderr)
             continue
 
