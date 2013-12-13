f10ff13c08fcc7c0363d262451df48c54e69abb8
Maarten van Gompel <proycon@anaproy.nl>
2013-12-13 22:06:29 +0100

    update

--- colibrimt/alignmentmodel.py
+++ colibrimt/alignmentmodel.py
@@ -665,9 +665,11 @@ def main_extractfeatures():
     parser.add_argument('-l','--leftsize',type=int,help="Left context size (may be specified multiple times, once per -f)", action='append',required=True)
     parser.add_argument('-r','--rightsize',type=int,help="Right context size (may be specified multiple times, once per -f)", action='append',required=True)
     parser.add_argument('-C','--buildclassifiers',help="Build classifier training data, one classifier expert per pattern, specify a working directory in -o", action='store_true',default=False)
-    parser.add_argument('-x','--weighbyoccurrence',help="When building classifier data (-C), use exemplar weighting to reflect occurrence count, rather than duplicating instances", action='store_true',default=False)
-    parser.add_argument('-X','--weighbyscore',help="When building classifier data (-C), use exemplar weighting to weigh in p(t|s) from score vector", action='store_true',default=False)
+    parser.add_argument('-w','--weighbyoccurrence',help="When building classifier data (-C), use exemplar weighting to reflect occurrence count, rather than duplicating instances", action='store_true',default=False)
+    parser.add_argument('-W','--weighbyscore',help="When building classifier data (-C), use exemplar weighting to weigh in p(t|s) from score vector", action='store_true',default=False)
     parser.add_argument('-I','--instancethreshold',type=int,help="Classifiers (-C) having less than the specified number of instances will be not be generated", action='store',default=2)
+    parser.add_argument('-X','--experts', help="Classifier experts, one per source pattern", action="store_true", default=False)
+    parser.add_argument('-M','--monolithic', help="Monolithic classifier", action="store_true", default=False)
     args = parser.parse_args()
 
     if not (len(args.corpusfile) == len(args.classfile) == len(args.leftsize) == len(args.rightsize)):
