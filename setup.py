e202f9507b0ce7f386c1f1f6c46747fea7926a7a
Maarten van Gompel <proycon@anaproy.nl>
2013-12-23 15:28:01 +0100

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name = "colibrimt",
-    version = "0.1.2",
+    version = "0.1.3",
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("Colibri MT"),
