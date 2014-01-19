7696666cb4f0ea2321d63be75bcf220ec7140a9e
Maarten van Gompel <proycon@anaproy.nl>
2014-01-19 19:48:24 +0100

    debug

--- scripts/colibri-mt-exp.sh
+++ scripts/colibri-mt-exp.sh
@@ -26,6 +26,7 @@ fi
 TWEIGHTS_COMMA=""
 TWEIGHTS_OPTIONS=""
 for tweight in ${TWEIGHTS[*]}; do
+    echo $tweight
     if [ ! -z "${TWEIGHTS_COMMA}" ]; then
         TWEIGHTS_COMMA="${TWEIGHTS_COMMA},${tweight}"
     else
