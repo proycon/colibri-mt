919a9c0dcca7efccaaafbff81f78c1712e49cef1
Maarten van Gompel <proycon@anaproy.nl>
2014-03-19 16:18:27 +0100

    fix

--- scripts/colibri-mt-exp.sh
+++ scripts/colibri-mt-exp.sh
@@ -160,7 +160,7 @@ if [ "$MOSESONLY" = "1" ]; then
 
     if [ "$MERT" = 1 ]; then
         echo -e "${blue}[$NAME (Moses only)]\nRunning MERT${NC}">&2
-        CMD="$MOSESDIR/scripts/training/mert-moses.pl --decoder-flags="-threads $THREADS" --mertdir=$MOSESDIR/mert/ ../$DEVSOURCE.txt ../$DEVTARGET.txt `which moses` model/moses.ini"
+        CMD="$MOSESDIR/scripts/training/mert-moses.pl --decoder-flags=\"-threads $THREADS\" --mertdir=$MOSESDIR/mert/ ../$DEVSOURCE.txt ../$DEVTARGET.txt `which moses` model/moses.ini"
         echo $CMD>&2
         $CMD
         if [[ $? -ne 0 ]]; then
